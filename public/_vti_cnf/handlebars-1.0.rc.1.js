64db853185153abe8641b5bb7fc28680d5930bb4
eibhlin <chaos.jestem@gmail.com>
2013-02-16 00:53:29 +0100

    start

---
+++ public/_vti_cnf/handlebars-1.0.rc.1.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|16 Jan 2013 08:57:59 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|smieciowki.pl/b9b34730a7beff5b632f9d52e0214340/index.htm
---
+++ public/_vti_cnf/jquery.min.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|16 Jan 2013 01:07:02 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|smieciowki.pl/b9b34730a7beff5b632f9d52e0214340/index.htm
